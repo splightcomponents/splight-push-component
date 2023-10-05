274b3f499a32f9a447be1cd6681015203b852c88
franco <francozacco@gmail.com>
2023-10-05 13:19:56 +0200

    testing action

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-echo $INPUT_SPLIGHT_CLI_VERSION
-echo $SPLIGHT_CLI_VERSION
 
-pip install splight-cli==$SPLIGHT_CLI_VERSION
+pip install splight-cli==$INPUT_SPLIGHT_CLI_VERSION
 python3 entrypoint.py
